92a87ce0c421642cd2e77d2fab0471f96c04f201
unknown <kitos200387@gmail.com>
2017-11-07 15:59:54 +0600

    PreRelise

--- script.js
+++ script.js
@@ -3,17 +3,30 @@ var disp = 0;
 var eq = [];
 var ans = 0;
 
-function display(eq){//displays result on "screen"
-  disp=eq;
+function display(c){//displays result on "screen"
+  disp=c;
   document.getElementById("display").innerHTML=disp;
 }
 
 function solve(a){
+  var last = a[a.length()-1];
+  switch(last){
+    case("="):{
 
+    }
+    case("sqrt"):{
+      ans = Math.sqrt(a[a.length()-2]);
+      a.pop();
+      a.pop();
+      a.push(ans);
+      display(ans);
+    }
+  }
 }
 
 function pressCount(a){
   display((disp*10)+a);
+  eq.push(a);
 }
 
 function pressOp(a){
