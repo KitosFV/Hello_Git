1d8d25931079ea1127cc6be3dce70c1578d4e2b0
KitosFV <kitos200387@gmail.com>
2017-11-11 20:03:54 +0300

    Changed to 0.8 version

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var mem = 0;
 var disp = 0;
 var eq = [];
 var ans = 0;
+var ad = -1;
+var pr = ["*","/","mod","+","-"];
 
 function display(c){//displays result on "screen"
   disp=c;
@@ -9,27 +11,77 @@ function display(c){//displays result on "screen"
 }
 
 function solve(a){
-  var last = a[a.length()-1];
+  var last = eq[eq.length-1];
   switch(last){
     case("="):{
-
+      eq.pop;
+      for(var i=0;i<5;i++){
+        while(eq.indexOf(pr[i])!=-1){
+          var smEq = [eq[eq.indexOf(pr[i])-1],eq.indexOf(pr[i]),eq.indexOf(pr[i]+1)];
+          switch(pr[i]){
+            case("*"):{
+              ans = smEq[0]*smEq[2];
+            }
+            case("/"):{
+              ans = smEq[0]/smEq[2];
+            }
+            case("+"):{
+              ans = smEq[0]+smEq[2];
+            }
+            case("-"):{
+              ans = smEq[0]-smEq[2];
+            }
+            case("mod"):{
+              ans = smEq[0]%smEq[2];
+            }
+          }
+          eq[eq.indexOf(pr[i])]=ans;
+          delete eq[eq.indexOf(pr[i])+1];
+          delete eq[eq.indexOf(pr[i])-1];
+          display(ans);
+        }
+      }
+      display(ans);
+      break;
     }
     case("sqrt"):{
-      ans = Math.sqrt(a[a.length()-2]);
-      a.pop();
-      a.pop();
-      a.push(ans);
+      ans = Math.pow(eq[eq.length-2],1/2);
+      eq.pop;
+      eq.pop;
+      eq.push(ans);
       display(ans);
+      break;
+    }
+    case("pm"):{
+      ans = -(eq[eq.length-2]);
+      eq.pop;
+      eq.pop;
+      eq.push(ans);
+      display(ans);
+      break;
+    }
+    case("dot"):{
+      if(ad==-1){
+        ad=10;
+      }
+      display(disp);
+      break;
     }
   }
 }
 
 function pressCount(a){
-  display((disp*10)+a);
-  eq.push(a);
+  if(ad==-1){
+    display((disp*10)+a);
+  }else{
+    display(disp+(a/ad));
+    ad*=10;
+  }
 }
 
 function pressOp(a){
+  eq.push(disp);
   eq.push(a);
+  ad=-1;
   solve(eq);
 }
